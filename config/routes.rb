2887469b0d2529be9c454715a8ec3af4ff586674
Sanyath Thammawong <sanyath007@gmail.com>
2020-07-28 12:24:50 +0700

    Added User and Session module and edited route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  
   get 'welcome/index'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions/new', as: 'login'
+  get 'logout', to: 'sessions/destroy', as: 'logout'
+  
+  resources :users
+  resources :sessions, only: [:new, :create :destory]
   
   resources :articles do
     resources :comments
   end
-
-  root 'welcome#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_084505) do
+ActiveRecord::Schema.define(version: 2020_07_28_050006) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2020_07_18_084505) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "comments", "articles"
